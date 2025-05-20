dff628bf9334552aad85f4f48f6ecc41a6f82f2e
user <carilinemarceano@albassort.com>
2025-05-20 02:54:26 -0400

    initial commit

---
+++ groupby.nim
@@ -0,0 +1,22 @@
+import tables
+import sugar
+import sequtils
+
+func groupBy*[A,B,C](a : openArray[A],
+                    b : proc(a : A) : B {.closure.},
+                    c : proc(a : A) : C {.closure.}) : Table[B, seq[C]] {.inline, effectsOf: b, effectsOf: c.} =
+  for x in a:
+    let bOfX = b(x)
+    let cOfX = c(x)
+    if bOfX notin result:
+      result[bOfX] = @[cOfX]
+    else:
+      result[bOfX].add(cOfX)
+
+proc keyVal*[A,B,C](a : openArray[A],
+        b : proc(a : A) : B {.closure.},
+        c : proc(a : A) : C {.closure.}) : Table[B, C] {.inline, effectsOf: b, effectsOf: c.} =
+  for x in a:
+    let bOfX = b(x)
+    let cOfX = c(x)
+    result[bOfX] = cOfX
---
+++ groupby.nimble
@@ -0,0 +1,6 @@
+version       = "0.0.0"
+author        = "Caroline Marceano [Albassort]"
+description   = "A simple functional programming utility in Nim."
+license       = "MIT"
+requires "nim >= 2.0.6"
+
